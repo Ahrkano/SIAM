157f0e7a2c0f41636a103671c65f4a669f5a1699
Ahrkano <36827623+Ahrkano@users.noreply.github.com>
2020-02-04 11:48:55 -0300

    adicionados permissoes e logs

---
+++ app/config/siam.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host'  =>  "127.0.0.1",
+    'port'  =>  "",
+    'name'  =>  "siam",
+    'user'  =>  "root",
+    'pass'  =>  "P12510245",
+    'type'  =>  "mysql",
+    'prep'  =>  "1"
+];
